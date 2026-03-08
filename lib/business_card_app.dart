3bf71209b77557f6e1f7b5cac56d91309ae9750c
Khaled Sameh <eng.khaled.sameh.360@gmail.com>
2026-03-08 06:02:36 +0200

    first commit

---
+++ lib/business_card_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BusinessCardApp extends StatelessWidget {
+  const BusinessCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:business_card/business_card_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BusinessCardApp());
+}
